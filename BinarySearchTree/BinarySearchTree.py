b01cb0ddfbe2478e82fe19601b17661b247a82b8
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-02-01 19:20:54 -0300

    feat: searchRecursive and searchIterative

--- BinarySearchTree/BinarySearchTree.py
+++ BinarySearchTree/BinarySearchTree.py
@@ -2,21 +2,17 @@ from .Node import Node
 class BinarySearchTree:
     def __init__(self):
         self.root: Node = None
-        self.left: Node = None
-        self.right: Node = None
 
     def add(self, value) -> None:
         # A arvore está vazia
         if self.root is None:
             self.root = Node(value, None, None)
-
         else:
             currentNode: Node = self.root
 
             while currentNode.value is not value:
                 if value < currentNode.value:
                     if currentNode.left is None:
-                        print(f"O valor {value} foi adicionado à esquerda de {currentNode.value}")
                         currentNode.left = Node(value, None, None)
                         currentNode = currentNode.left
                     else:
@@ -24,8 +20,46 @@ class BinarySearchTree:
 
                 elif value > currentNode.value:
                     if currentNode.right is None:
-                        print(f"O valor {value} foi adicionado à direita de {currentNode.value}")
                         currentNode.right = Node(value, None, None)
                         currentNode = currentNode.right
                     else:
                         currentNode = currentNode.right
+
+    def searchIterative(self, value):
+        if self.root is None:
+            print("Raiz é none")
+            return False
+
+        currentNode = self.root
+        while currentNode is not None:
+            print(currentNode.value)
+            if currentNode.value == value:
+                return True
+            
+            else:
+                if value < currentNode.value:
+                    currentNode = currentNode.left
+
+                elif value > currentNode.value:
+                    currentNode = currentNode.right
+        
+        return False
+    
+    def searchRecursive(self, currentNode, value):
+        if currentNode is None:
+            return False
+
+        if currentNode.value == value:
+            return True
+        else:
+            if value < currentNode.value:
+                return self.searchRecursive(currentNode.left, value)
+            elif value > currentNode.value:
+                return self.searchRecursive(currentNode.right, value)
+        
+    def remove(self, value):
+        # O valor não está na lista
+        # O valor está no primeiro nó
+        # Valor está em um galho
+        # O valor está nas folhas
+        pass
