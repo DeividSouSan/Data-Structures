daf00ccf99cdb81733ca621a89d52a2a318e1e84
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-05-12 15:55:59 -0300

    feat: :zap: Added start and end attribute.
    The start and end attribute will improve perfomance cause the methods won't need to search for the last element.

--- Queues/data_structure/queue.py
+++ Queues/data_structure/queue.py
@@ -1,9 +1,18 @@
 class Queue[T]:
     def __init__(self):
         self.items: list[T] = []
-        self.size = 0
+        self.start: T = None
+        self.end: T = None
 
+        self.size = 0
+        
     def enqueue(self, item: T) -> None:
+        if self.size == 0:
+            self.start = item
+            self.end = item
+        else:
+            self.end = item
+            
         self.items.append(item)
         self.size += 1
 
@@ -12,10 +21,17 @@ class Queue[T]:
             raise EmptyQueue("Can't deque from empty queue.")
         
         self.size -= 1
+        
+        if self.size == 0:
+            self.start = None
+            self.end = None
+        else:
+            self.start = self.items[1]
+            
         return self.items.pop(0)
 
     def peek(self) -> T:
-        return self.items[0]
+        return self.start
 
     def is_empty(self) -> bool:
         return len(self.items) == 0
